f7576e928908e9f949216d7e9e8f172a8d2ed1ba
Stefano Chermaz <stefano.chermaz@gmail.com>
2025-08-09 13:54:09 +0200

    ripristinato login admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->login()
             ->pages([
                 Pages\Dashboard::class,
             ])
