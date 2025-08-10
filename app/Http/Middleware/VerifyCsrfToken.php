70c4e6c3ac55253c5bf30fd80f5c3c955b655746
Stefano Chermaz <stefano.chermaz@gmail.com>
2025-08-10 10:55:25 +0200

    Sitemap

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -14,5 +16,25 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         'api/cats/*/like',
         'api/*',
+        'sitemap.xml',
+        'robots.txt',
     ];
+
+    /**
+     * Avoid setting XSRF cookie when there is no session available or for sitemap/robots routes.
+     *
+     * @param  SymfonyResponse  $response
+     * @param  Request          $request
+     * @return SymfonyResponse
+     */
+    protected function addCookieToResponse($response, $request)
+    {
+        if ($request instanceof Request) {
+            if (!$request->hasSession() || $request->is('sitemap.xml') || $request->is('robots.txt')) {
+                return $response;
+            }
+        }
+
+        return parent::addCookieToResponse($response, $request);
+    }
 }
