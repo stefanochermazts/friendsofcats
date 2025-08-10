96bae02d7e85f5db5a43b1762d35b9f3cb9296dc
Stefano Chermaz <stefano.chermaz@gmail.com>
2025-08-10 11:01:08 +0200

    Sitemap

--- resources/views/sitemap/xml.blade.php
+++ resources/views/sitemap/xml.blade.php
@@ -1,4 +1,4 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n"; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xhtml="http://www.w3.org/1999/xhtml">
 @foreach($urls as $u)
